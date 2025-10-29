b81609e4b5f630cf4c0cd07ae00752c5a68bcea5
Pablo Andrés Márquez Fernández <pmarquez@desarrolloeconomico.gov.co>
2025-10-28 19:13:53 -0500

    Despliegue en K8S

--- tools/execute_query.py
+++ tools/execute_query.py
@@ -1,6 +1,6 @@
 # tools/execute_query.py
 from pydantic import BaseModel, ConfigDict
-from database.connection import execute_sql
+# from database.connection import execute_sql
 
 class ExecuteQueryRequest(BaseModel):
     query: str
@@ -8,14 +8,14 @@ class ExecuteQueryRequest(BaseModel):
 
 
 
-def execute_query(request: ExecuteQueryRequest) -> list[dict]:
-    """
-    Ejecuta un query SQL y retorna los resultados en formato lista de diccionarios.
-    Usa con precaución: solo acepta SELECTs u operaciones seguras.
-    """
-    # Seguridad básica para evitar operaciones destructivas
-    lowered = request.query.strip().lower()
-    if any(keyword in lowered for keyword in ["drop", "delete", "update", "insert", "alter"]):
-        return [{"error": "Solo se permiten consultas SELECT seguras"}]
+# def execute_query(request: ExecuteQueryRequest) -> list[dict]:
+#     """
+#     Ejecuta un query SQL y retorna los resultados en formato lista de diccionarios.
+#     Usa con precaución: solo acepta SELECTs u operaciones seguras.
+#     """
+#     # Seguridad básica para evitar operaciones destructivas
+#     lowered = request.query.strip().lower()
+#     if any(keyword in lowered for keyword in ["drop", "delete", "update", "insert", "alter"]):
+#         return [{"error": "Solo se permiten consultas SELECT seguras"}]
     
-    return execute_sql(request.query)
+#     return execute_sql(request.query)
