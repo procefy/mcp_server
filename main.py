190049dd257a82d7d1e76a4728e04753f11877c3
Pablo Andrés Márquez Fernández <pmarquez@desarrolloeconomico.gov.co>
2025-10-28 19:24:03 -0500

    Despliegue en K8S

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # main.py
+from typing import Union
 from fastmcp import FastMCP
 from database.connection import describe_db, execute_sql, get_engine
 from tools.execute_query import ExecuteQueryRequest
+# from tools.execute_query import ExecuteQueryRequest
 
 # Crea el servidor FastMCP
 app = FastMCP("DBAgentServer", version="0.1.0")
@@ -9,7 +11,8 @@ app = FastMCP("DBAgentServer", version="0.1.0")
 # Registra las tools
 
 @app.tool
-def execute_query(execute: ExecuteQueryRequest) -> list[dict]:
+def execute_query(request: Union[ExecuteQueryRequest, dict]) -> list[dict]:
+    print("Datos recibidos:", request)
 # def execute_query(request: ExecuteQueryRequest) -> list[dict]:
     """
     Ejecuta un query SQL y retorna los resultados en formato lista de diccionarios.
